adb6a70c840fb6bdbd7f095bffa4e971adfc192d
thinhbo19 <111267094+thinhbo19@users.noreply.github.com>
2024-07-23 11:48:19 +0700

    a

--- app/(admin)/dashboard/san-pham/[productId]/page.js
+++ app/(admin)/dashboard/san-pham/[productId]/page.js
@@ -1,8 +1,5 @@
-import { getBrand, getCategory } from "@/services/Redux/fetchData/useFetchData";
 import EditForm from "./EditForm";
 
 export default async function EditProductPage() {
-  const brand = await getBrand();
-  const Category = await getCategory();
-  return <EditForm brand={brand} Category={Category} />;
+  return <EditForm />;
 }
