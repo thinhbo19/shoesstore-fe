6a698cb541464d09357693034a973abb7b3f05da
thinhbo19 <111267094+thinhbo19@users.noreply.github.com>
2024-08-06 20:01:10 +0700

    up

--- app/(user)/san-pham/[productName]/layout.js
+++ app/(user)/san-pham/[productName]/layout.js
@@ -1,9 +1,10 @@
-import { getCategory } from "@/services/Redux/fetchData/useFetchData";
+import { getAllProducts } from "@/services/Redux/api";
 import { headers } from "next/headers";
 
 export async function generateMetadata({ params, searchParams }, parent) {
-  const { categoryName } = params;
-  const data = await getCategory();
+  const { productName } = params;
+
+  const data = await getAllProducts();
 
   function removeVietnameseTones(str) {
     return str
@@ -11,16 +12,17 @@ export async function generateMetadata({ params, searchParams }, parent) {
       .replace(/[\u0300-\u036f]/g, "")
       .replace(/đ/g, "d")
       .replace(/Đ/g, "D")
+      .replace(/\s+/g, "-")
       .toLowerCase();
   }
-  let matchedCategoryName = categoryName;
+  let matchedName = productName;
 
   data.forEach((item) => {
     const normalizedItemName = removeVietnameseTones(
-      item.categoryName.toLowerCase()
+      item.productName.toLowerCase()
     );
-    if (categoryName === normalizedItemName) {
-      matchedCategoryName = item.categoryName.toLowerCase();
+    if (productName === normalizedItemName) {
+      matchedName = item.productName;
     }
   });
 
@@ -28,7 +30,7 @@ export async function generateMetadata({ params, searchParams }, parent) {
   const header_url = headersList.get("x-url") || "";
   const domain = headersList.get("host") || "";
   return {
-    title: `Danh mục ${matchedCategoryName} - Shoes Store`,
+    title: `${matchedName} - Shoes Store`,
     description:
       "Chào mừng đến với cửa hàng bán giày chính hãng. Chúng tôi cung cấp giày chất lượng cao với giá tốt nhất.",
     alternates: {
@@ -37,7 +39,7 @@ export async function generateMetadata({ params, searchParams }, parent) {
     metadataBase:
       "https://shoesstore-thinhbo19s-projects.vercel.app/danh-muc-san-pham/",
     openGraph: {
-      title: `Danh mục ${matchedCategoryName} - Shoes Store`,
+      title: `${matchedName} - Shoes Store`,
       description:
         "Chào mừng đến với cửa hàng bán giày chính hãng. Chúng tôi cung cấp giày chất lượng cao với giá tốt nhất.",
       url: header_url,
@@ -51,6 +53,6 @@ export async function generateMetadata({ params, searchParams }, parent) {
   };
 }
 
-export default function CategoryLayout({ children }) {
+export default function DetailProductsLayout({ children }) {
   return <>{children}</>;
 }
--- app/(user)/san-pham/layout.js
+++ app/(user)/san-pham/layout.js
@@ -1,3 +1,32 @@
+import { headers } from "next/headers";
+
+export async function generateMetadata({ params, searchParams }, parent) {
+  const headersList = headers();
+  const header_url = headersList.get("x-url") || "";
+  const domain = headersList.get("host") || "";
+  return {
+    title: "Sản phẩm - Cửa hàng bán giày",
+    description:
+      "Chào mừng đến với cửa hàng bán giày chính hãng. Chúng tôi cung cấp giày chất lượng cao với giá tốt nhất.",
+    alternates: {
+      canonical: "./",
+    },
+    metadataBase: "https://shoesstore-thinhbo19s-projects.vercel.app/san-pham",
+    openGraph: {
+      title: "Sản phẩm - Cửa hàng bán giày",
+      description:
+        "Chào mừng đến với cửa hàng bán giày chính hãng. Chúng tôi cung cấp giày chất lượng cao với giá tốt nhất.",
+      url: header_url,
+      siteName: domain,
+      images: [
+        {
+          url: "https://res.cloudinary.com/dq1bmcdyc/image/upload/v1722946177/imageLogin_ktbiup.png",
+        },
+      ],
+    },
+  };
+}
+
 export default function ProductsLayout({ children }) {
   return <>{children}</>;
 }
