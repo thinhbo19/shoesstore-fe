dc7d8b92624d2b519ece14aabfa5205787a2702f
thinhbo19 <111267094+thinhbo19@users.noreply.github.com>
2024-08-11 23:01:04 +0700

    add

--- utils/hanleGet.js
+++ utils/hanleGet.js
@@ -3,6 +3,7 @@ import { setCateID, setProductId } from "@/services/Redux/product/productSlice";
 export const handleProductID = (dispatch, productID) => {
   dispatch(setProductId(productID));
 };
+
 export const handleCateID = (dispatch, categoryID) => {
   dispatch(setCateID(categoryID));
 };
