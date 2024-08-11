910bb6abfac12b13eaff408e19d377409aea5ff5
thinhbo19 <111267094+thinhbo19@users.noreply.github.com>
2024-08-11 23:57:29 +0700

    cont

--- app/layout.js
+++ app/layout.js
@@ -33,6 +33,19 @@ export async function generateMetadata({ params, searchParams }) {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script id="mcjs">
+          !function(c,h,i,m,p)
+          {
+            ((m = c.createElement(h)),
+            (p = c.getElementsByTagName(h)[0]),
+            (m.async = 1),
+            (m.src = i),
+            p.parentNode.insertBefore(m, p))
+          }
+          (document,"script","https://chimpstatic.com/mcjs-connected/js/users/fec924f00947bba359f746e97/ce9444df0b7db8327aeb2cb75.js");
+        </script>
+      </head>
       <body>
         <Script
           async
